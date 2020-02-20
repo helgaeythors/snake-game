957c088cdc54890fad143b9c76f4e269ad0fecf6
unknown <helga.ey@gmail.com>
2020-02-20 22:35:39 +0000

    refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders snake element', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/snake/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders play button', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/play/i);
   expect(linkElement).toBeInTheDocument();
 });
